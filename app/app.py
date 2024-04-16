243fd0fb8de08fb8fb35af7201b52c46c4270815
Brennan Lehman <blehman@nwmissouri.edu>
2024-04-16 09:32:51 -0500

    updates to app file

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+#Add in imports
 import seaborn as sns
 from faicons import icon_svg
 
@@ -7,73 +8,76 @@ import palmerpenguins
 
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+ui.page_opts(title="xxxPenguins dashboard", fillable=True) #this is the title of the page
 
+    
 
-with ui.sidebar(title="Filter controls"):
+with ui.sidebar(title="Filter controls", style="background-color: lightgray;"): #this is the title of the sidebar
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
     ui.input_checkbox_group(
         "species",
-        "Species",
+        "xxxSpecies",
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie", "Gentoo", "Chinstrap"],
     )
     ui.hr()
+
+    #this section is for the links
     ui.h6("Links")
     ui.a(
-        "GitHub Source",
+        "xxxGitHub Source",
         href="https://github.com/denisecase/cintel-07-tdash",
         target="_blank",
     )
     ui.a(
-        "GitHub App",
+        "xxxGitHub App",
         href="https://denisecase.github.io/cintel-07-tdash/",
         target="_blank",
     )
     ui.a(
-        "GitHub Issues",
+        "xxxGitHub Issues",
         href="https://github.com/denisecase/cintel-07-tdash/issues",
         target="_blank",
     )
-    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+    ui.a("xxxPyShiny", href="https://shiny.posit.co/py/", target="_blank")
     ui.a(
         "Template: Basic Dashboard",
         href="https://shiny.posit.co/py/templates/dashboard/",
         target="_blank",
     )
     ui.a(
-        "See also",
+        "xxxSee also",
         href="https://github.com/denisecase/pyshiny-penguins-dashboard-express",
         target="_blank",
     )
 
 
-with ui.layout_column_wrap(fill=False):
+with ui.layout_column_wrap(fill=False, style="background-color: lightgray;"): #this is the title of the column wrap
     with ui.value_box(showcase=icon_svg("earlybirds")):
-        "Number of penguins"
+        "xxxNumber of penguins"
 
         @render.text
         def count():
             return filtered_df().shape[0]
 
     with ui.value_box(showcase=icon_svg("ruler-horizontal")):
-        "Average bill length"
+        "xxxAverage bill length"
 
         @render.text
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
     with ui.value_box(showcase=icon_svg("ruler-vertical")):
-        "Average bill depth"
+        "xxxAverage bill depth"
 
         @render.text
         def bill_depth():
             return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
 
 
-with ui.layout_columns():
-    with ui.card(full_screen=True):
-        ui.card_header("Bill length and depth")
+with ui.layout_columns(): #this is the title of the column
+    with ui.card(full_screen=True, style="background-color: lightgray;"):
+        ui.card_header("xxxBill length and depth")
 
         @render.plot
         def length_depth():
@@ -84,7 +88,7 @@ with ui.layout_columns():
                 hue="species",
             )
 
-    with ui.card(full_screen=True):
+    with ui.card(full_screen=True, style="background-color: lightgray;"): #this is the title of the card
         ui.card_header("Penguin Data")
 
         @render.data_frame
